7acb710e625700c0903c26d9550c0b01ec90ad27
Matt Nicks <matthew.nicks@methods.co.uk>
2021-12-10 10:16:06 +0000

    add basic mapper for the area model

--- mapper/mapper.go
+++ mapper/mapper.go
@@ -22,3 +22,39 @@ func CreateStartPage(basePage coreModel.Page) StartPageModel {
 	model.Page.BetaBannerEnabled = true
 	return model
 }
+
+type AreaModel struct {
+	coreModel.Page
+	Name string `json:"name"`
+	Level string `json:"level"`
+	Code string `json:"code"`
+	Ancestors []AreaModel `json:"ancestors"`
+	Siblings []AreaModel `json:"siblings"`
+	Children []AreaModel `json:"children"`
+}
+
+func CreateAreaPage(basePage coreModel.Page) AreaModel {
+	// TODO - load the area data for the requested area once the API has been developed
+	model := AreaModel{
+		Page: basePage,
+	}
+	model.Metadata = coreModel.Metadata{
+		Title: model.Name + " Summary",
+	}
+	model.Page.Breadcrumb = append(model.Page.Breadcrumb, coreModel.TaxonomyNode{
+		Title: "Home",
+		URI:   "/",
+	})
+	model.Page.Breadcrumb = append(model.Page.Breadcrumb, coreModel.TaxonomyNode{
+		Title: "Areas",
+		URI:   "/areas",
+	})
+	for _, ancestor := range model.Ancestors {
+		model.Page.Breadcrumb = append(model.Page.Breadcrumb, coreModel.TaxonomyNode{
+			Title: ancestor.Name,
+			URI:   "/areas/" + ancestor.Code,
+		})
+	}
+	model.Page.BetaBannerEnabled = true
+	return model
+}
