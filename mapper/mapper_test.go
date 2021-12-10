777cbfec084a0100162ab25734135a5db384d16b
Matt Nicks <matthew.nicks@methods.co.uk>
2021-12-10 17:24:10 +0000

    add handler and mapper tests

--- mapper/mapper_test.go
+++ mapper/mapper_test.go
@@ -19,4 +19,20 @@ func TestUnitMapper(t *testing.T) {
 		So(geoStartModel.Page.Breadcrumb[0].Title, ShouldEqual, "Home")
 		So(geoStartModel.Page.Breadcrumb[0].URI, ShouldEqual, "/")
 	})
+
+	Convey("test CreateAreaPage mapper correctly initialises an AreaModel", t, func() {
+		areaModel := CreateAreaPage(mdl)
+
+		So(areaModel.BetaBannerEnabled, ShouldBeTrue)
+		So(areaModel.Metadata.Title, ShouldEqual, areaModel.Name + " Summary")
+		So(len(areaModel.Page.Breadcrumb), ShouldEqual, len(areaModel.Ancestors) + 2)
+		So(areaModel.Page.Breadcrumb[0].Title, ShouldEqual, "Home")
+		So(areaModel.Page.Breadcrumb[0].URI, ShouldEqual, "/")
+		So(areaModel.Page.Breadcrumb[1].Title, ShouldEqual, "Areas")
+		So(areaModel.Page.Breadcrumb[1].URI, ShouldEqual, "/areas")
+		if len(areaModel.Ancestors) > 0 {
+			So(areaModel.Page.Breadcrumb[2].Title, ShouldEqual, areaModel.Ancestors[0].Name)
+			So(areaModel.Page.Breadcrumb[2].URI, ShouldEqual, "/areas/" + areaModel.Ancestors[0].Code)
+		}
+	})
 }
