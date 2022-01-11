856f9b3e0d9a065519c869780ff5661d709b492d
Peter Idah <peter.idah@gmail.com>
2022-01-11 17:42:28 +0000

    add google-chrome-stable

--- ci/scripts/component.sh
+++ ci/scripts/component.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -eux
 
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
+echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list
+apt-get update && apt-get -y install google-chrome-stable
 pushd dp-frontend-area-profiles
   make test-component
 popd
