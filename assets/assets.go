d5cbf6a05bdaf0e7a11b592e1e1b453b4ae4bd8a
Mariana Silva <Marianasilvago1234@gmail.com>
2022-07-05 21:43:12 +0530

    Replace the stock net/http server with a dpnet

--- assets/assets.go
+++ assets/assets.go
@@ -1,3 +1,21 @@
 // assets in locale and templates folders are converted into Go source code with go-bindata
 // the data.go file in this package is auto generated through the generate-debug and generate-prod tasks in the Makefile
 package assets
+
+import "io/fs"
+
+func Asset(name string) ([]byte, error) {
+	return []byte{}, nil
+}
+
+func AssetNames() []string {
+	return []string{}
+}
+
+func AssetDir(path string) ([]string, error) {
+	return []string{}, nil
+}
+
+func AssetInfo(path string) (fs.FileInfo, error) {
+	return nil, nil
+}
